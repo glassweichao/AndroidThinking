4a7ff6301a38c9f9fb0efddfa54ab8e200c119d3
Charles Wei <charles.wei1920@gmail.com>
2017-04-24 21:19:25 +0800

    #2017.4.24 fragment test

---
+++ app/src/main/java/com/charleswei/androidthinking/main.java
@@ -0,0 +1,17 @@
+package com.charleswei.androidthinking;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2017/4/24.
+ */
+
+public class main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+}
